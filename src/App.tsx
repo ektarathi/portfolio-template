4291be8c466c70e6f55fdfaba3c38e7cbbf00800
Ekta Rathi <er@arenko.group>
2023-05-03 23:29:53 +0100

    Updated landing page path structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import LandingPage from "./components/Landing/LandingComponent";
+import LandingPage from "./pages/LandingPage";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
-import Header from "../Banner/Header/HeaderComponent";
-import Profile from "../Banner/Profile/ProfileComponent";
 import Container from "@mui/material/Container";
-import Services from "../Service/Services";
 import styled from "@emotion/styled";
+
+//components
+import Header from "../components/Banner/Header/HeaderComponent";
+import Profile from "../components/Banner/Profile/ProfileComponent";
+import Services from "../components/Service/Services";
 
 const Banner = styled.div`
   @media (max-width: 640px) {
