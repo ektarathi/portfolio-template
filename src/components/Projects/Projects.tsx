8655c0453365958e176715cf583d9fc1bd7865f2
Ekta Rathi <er@arenko.group>
2023-05-25 22:37:44 +0100

    Updated styling

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,32 +1,36 @@
 import { Zoom } from "react-awesome-reveal";
-import { StyledWrapper } from "./projects.styled";
+import { StyledWrapper, StyledCircle } from "./projects.styled";
 import { Container, Typography, Box } from "@mui/material";
 import SliderComp from "./Slider/SlideComponent";
 
 const Projects = () => {
-
   return (
     <StyledWrapper id="project">
-      <Box style={{ paddingTop: "3rem", paddingBottom: "3rem" }}>
+      <Box style={{ paddingTop: "3rem" }}>
+        <Zoom>
+          <Typography variant="h5" component="h1" data-testid="project-title">
+            Recent{" "}
+            <Typography
+              variant="h5"
+              component="span"
+              style={{ color: "#01be96" }}
+            >
+              Projects
+            </Typography>
+          </Typography>
+          <Typography variant="body2" data-testid="title-text">
+            Showcase of the recent UI on which I have worked on
+          </Typography>
+        </Zoom>
+      </Box>
+      <Box style={{ marginLeft: 200 }}>
         <Container>
-          <Zoom>
-            <Typography variant="h5" component="h1" data-testid="project-title">
-              Recent{" "}
-              <Typography
-                variant="h5"
-                component="span"
-                style={{ color: "#01be96" }}
-              >
-                Projects
-              </Typography>
-            </Typography>
-            <Typography variant="body2" data-testid="title-text">
-              Showcase of the recent UI on which I have worked on
-            </Typography>
-          </Zoom>
           <SliderComp />
         </Container>
       </Box>
+      <StyledCircle>
+        <Box></Box>
+      </StyledCircle>
     </StyledWrapper>
   );
 };
--- src/components/Projects/projects.styled.ts
+++ src/components/Projects/projects.styled.ts
@@ -5,3 +5,18 @@ export const StyledWrapper = styled("div")((props: any) => ({
   position: 'relative',
   color: "#000000"
 }));
+
+export const StyledCircle = styled("div")((props: any) => ({
+  height: '400px',
+  width: '400px',
+  opacity: '1',
+  background: "white",
+  border: "30px solid #2999ad",
+  borderRadius: '50%',
+  position: "absolute",
+  bottom: '-9px',
+  left: '-230px',
+  zIndex: 200,
+}))
+
+//
